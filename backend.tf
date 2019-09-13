be67a9d1c6dd5512dc7a6e0dd1d698f4cca057f6
Matthew Van Stone <matt@Matthews-MacBook-Pro.local>
2019-09-12 23:37:37 -0600

    removed bucket and table attributes from common backend

--- backend.tf
+++ backend.tf
@@ -1,10 +1,6 @@
+# Additional backend config in *.tfbackend
 terraform {
   backend "s3" {
-    # Update to match bucket name defined in backend section of main.tf
-    bucket = "[pipeline-name]-dev-state-bucket"
-    key    = "terraform.tfstate"
-    # Update to match table name defined in backend section of main.tf
-    dynamodb_table = "a[pipeline-name]-dev-state-table"
-    region         = "us-east-1"
+      key = "terraform.tfstate"
   }
 }
